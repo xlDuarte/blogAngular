3869c14602df8ef7d29ae48e1a40ba5d49d2d03c
Gabriel Duarte <gabrielduarte_jardim@hotmail.com>
2022-01-18 17:25:40 -0300

    Add MENU CAD LOG RD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blogPessoal';
+  title = 'Bem vindo ao meu blogPessoal!';
+  idade = "T39";
 }
