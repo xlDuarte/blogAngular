85ecab28182991bb9d9fef47189631953ba8e88f
Gabriel Duarte <gabrielduarte_jardim@hotmail.com>
2022-01-24 17:40:10 -0300

    Componente inicio sem consumo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Bem vindo ao meu blogPessoal!';
-  idade = "T39";
+
+  constructor(
+    public auth: AuthService
+  ){}
+
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -14,11 +15,21 @@ export class AuthService {
   ) { }
 
   entrar(userlogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userlogin)
+    return this.http.post<UserLogin>('https://blogpessoalduarte.herokuapp.com/usuarios/logar', userlogin)
   }
 
   cadastrar(user: User): Observable<User>{
-    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
+    return this.http.post<User>('https://blogpessoalduarte.herokuapp.com/usuarios/cadastrar', user)
+  }
+
+  logado(){
+    let ok: boolean = false
+
+    if (environment.token != ''){
+      ok = true
+    }
+
+    return ok
   }
 
 }
