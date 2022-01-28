74ed4ccb2ebeede05519fc58a2faad3017c4b6a9
Gabriel Duarte <gabrielduarte_jardim@hotmail.com>
2022-01-28 18:33:12 -0300

    CrudPostPutUser

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -14,12 +14,26 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  refreshToken(){
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token)
+    }
+  }
+
   entrar(userlogin: UserLogin): Observable<UserLogin>{
     return this.http.post<UserLogin>('https://blogpessoalduarte.herokuapp.com/usuarios/logar', userlogin)
   }
 
   cadastrar(user: User): Observable<User>{
     return this.http.post<User>('https://blogpessoalduarte.herokuapp.com/usuarios/cadastrar', user)
+  }
+
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`https://blogpessoalduarte.herokuapp.com/usuarios/${id}`, this.token)
   }
 
   logado(){
